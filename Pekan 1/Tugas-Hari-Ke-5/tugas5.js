a729ba4f134dfdf2d08abc15168bcca55c9ef1d2
ariefrino50 <53212851+ariefrino50@users.noreply.github.com>
2020-07-24 23:31:21 +0700

    Add files via upload

---
+++ Pekan 1/Tugas-Hari-Ke-5/tugas5.js
@@ -0,0 +1,22 @@
+// Soal 1
+function halo(){
+    return 'Halo Sanbers!';
+}
+
+console.log(halo());
+
+
+// Soal 2
+function kalikan(a,b){
+    return a*b;
+}
+
+console.log(kalikan(10,10));
+
+
+// Soal 3
+function introduce(name, age, address, hobby){
+    return 'Nama saya ' + name + ', ' + 'umur saya ' + age + ' tahun,' + 'alamat saya di ' + address + ', dan saya punya hobby yaitu ' + hobby + '!';
+}
+
+console.log(introduce('Rino',23,'Jln agung barat 11', 'travelling'))
