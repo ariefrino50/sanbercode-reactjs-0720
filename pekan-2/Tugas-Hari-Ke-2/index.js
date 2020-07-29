67186bf6022a826e48f4a71cd5c2758fdef7553c
ariefrino50 <53212851+ariefrino50@users.noreply.github.com>
2020-07-29 07:19:51 +0700

    Add files via upload

---
+++ pekan-2/Tugas-Hari-Ke-2/index.js
@@ -0,0 +1,12 @@
+//soal 1
+// di index.js
+var readBooks = require('./callback.js')
+ 
+var books = [
+    {name: 'LOTR', timeSpent: 3000}, 
+    {name: 'Fidas', timeSpent: 2000}, 
+    {name: 'Kalkulus', timeSpent: 4000},
+    {name: 'komik', timeSpent: 1000}
+]
+ 
+// Tulis code untuk memanggil function readBooks di sini
---
+++ pekan-2/Tugas-Hari-Ke-2/index2.js
@@ -0,0 +1,10 @@
+//soal2
+var readBooksPromise = require('./promise.js')
+ 
+var books = [
+    {name: 'LOTR', timeSpent: 3000}, 
+    {name: 'Fidas', timeSpent: 2000}, 
+    {name: 'Kalkulus', timeSpent: 4000}
+]
+ 
+// Lanjutkan code untuk menjalankan function readBooksPromise 
